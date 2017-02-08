25aebf13f517e70c8c65a9369bc3f96e33f87f1e
Charlie Cuddy <charliecuddy4@gmail.com>
2017-02-08 06:47:21 +0000

    have numbers appear on screen once, when clicked

--- script.js
+++ script.js
@@ -1,3 +1,59 @@
-function someFunction() {
-    document.getElementById("screen").innerHTML = "1";
+var numbers = {
+    displayZero: function() {
+    var button = document.getElementById("zero").value;
+    //var displayScreen = document.getElementById("screen").value = button;
+    document.getElementById("screen").innerHTML = button;
+    },
+    
+    displayOne: function() {
+    var button = document.getElementById("one").value;
+    //var displayScreen = document.getElementById("screen").value = button;
+    document.getElementById("screen").innerHTML = button;
+    },
+    
+    displayTwo: function() {
+    var button = document.getElementById("two").value;
+    document.getElementById("screen").innerHTML = button;
+    },
+    
+    displayThree: function() {
+    var button = document.getElementById("three").value;
+    document.getElementById("screen").innerHTML = button;
+    },
+    
+    displayFour: function() {
+    var button = document.getElementById("four").value;
+    document.getElementById("screen").innerHTML = button;
+    },
+    
+    displayFive: function() {
+    var button = document.getElementById("five").value;
+    document.getElementById("screen").innerHTML = button;
+    },
+    
+    displaySix: function() {
+    var button = document.getElementById("six").value;
+    document.getElementById("screen").innerHTML = button;
+    },
+    
+    displaySeven: function() {
+    var button = document.getElementById("seven").value;
+    document.getElementById("screen").innerHTML = button;
+    },
+    
+    displayEight: function() {
+    var button = document.getElementById("eight").value;
+    document.getElementById("screen").innerHTML = button;
+    },
+    
+    displayNine: function() {
+    var button = document.getElementById("nine").value;
+    document.getElementById("screen").innerHTML = button;
+    },
+};
+
+
+var operators = {
+    
 }
+
